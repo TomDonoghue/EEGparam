ac226d1ca994ba2042209bc3b5aa3a93e7f148ab
TomDonoghue <tdonoghue@ucsd.edu>
2018-09-18 08:45:27 -0700

    Update some plots, move some uitils

--- notebooks/plts.py
+++ notebooks/plts.py
@@ -16,7 +16,7 @@ from utils import get_intersect
 YNG_INDS = range(14, 31)
 OLD_INDS = range(0, 14)
 
-YOU_COL = "#0d82c1"
+YNG_COL = "#0d82c1"
 OLD_COL = "#239909"
 
 ###################################################################################################
@@ -50,7 +50,7 @@ def plot_alpha_response_compare(canonical_alpha, fooofed_alpha, t_win, srate):
     ax1.legend(); ax2.legend();
 
 
-def plot_comp_boxplot(dat):
+def plot_comp_boxplot(dat, save_fig=False, save_name=None):
     """Plot comparison between groups, as a boxplot.
 
     Dat should be 1D vector, with data that can be split up by YOUNG & OLD _INDS.
@@ -69,12 +69,8 @@ def plot_comp_boxplot(dat):
                         patch_artist=True,
                         labels=['Young', 'Old'])
 
-    # Fill boxplots with colors
-    YOU_COL = "#0d82c1"
-    OLD_COL = "#239909"
-
     # Set colors of the boxes
-    colors = [YOU_COL, OLD_COL]
+    colors = [YNG_COL, OLD_COL]
     for box, color in zip(bplot['boxes'], colors):
         box.set_facecolor(color)
 
@@ -83,15 +79,9 @@ def plot_comp_boxplot(dat):
     plt.setp(ax.get_yticklabels(), fontsize=12)
 
     # Set the top and right side frame & ticks off
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
+    _set_lr_spines(ax, 2)
 
-    # Set linewidth of remaining spines
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['bottom'].set_linewidth(2)
-
+    _save_fig(save_fig, save_name)
 
 def plot_comp(dat, save_fig=False, save_name=None):
     """Plot comparison between groups, as a mean value with an errorbar.
@@ -120,18 +110,43 @@ def plot_comp(dat, save_fig=False, save_name=None):
 #    ax.set_ylabel('Error')
 
     # Set the top and right side frame & ticks off
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
+    _set_lr_spines(ax, 2)
 
-    # Set linewidth of remaining spines
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['bottom'].set_linewidth(2)
+    _save_fig(save_fig, save_name)
 
-    if save_fig:
-        save_name = 'plts/' + save_name + '.pdf'
-        plt.savefig(save_name, bbox_inches='tight', dpi=300)
+
+def plot_comp_scatter(data, label=None, save_fig=False, save_name=None):
+    """   """
+
+    fig, ax = plt.subplots(figsize=[2, 4])
+
+    x1, x2 = 0.5, 1.5
+    d1 = data[YNG_INDS]
+    d2 = data[OLD_INDS]
+
+    # Create x-axis data, with small jitter for visualization purposes
+    x_data_1 = np.ones_like(d1) * x1 + np.random.normal(0, 0.025, d1.shape)
+    x_data_2 = np.ones_like(d2) * x2 + np.random.normal(0, 0.025, d2.shape)
+
+    ax.scatter(x_data_1, d1, s=36, alpha=0.5, c=YNG_COL)
+    ax.plot([x1-0.2, x1+0.2], [np.mean(d1), np.mean(d1)], lw=5, c=YNG_COL)
+
+    ax.scatter(x_data_2, d2, s=36, alpha=0.5, c=OLD_COL)
+    ax.plot([x2-0.2, x2+0.2], [np.mean(d2), np.mean(d2)], lw=5, c=OLD_COL)
+
+    if label:
+        ax.set_ylabel(label, fontsize=16)
+
+    plt.xlim([0, 2])
+
+    plt.xticks([x1, x2], ["Young", "Old"])
+
+    ax.tick_params(axis='x', labelsize=12)
+    ax.tick_params(axis='y', labelsize=10)
+
+    _set_lr_spines(ax)
+
+    _save_fig(save_fig, save_name)
 
 
 def plot_oscillations(alphas, save_fig=False, save_name=None):
@@ -168,54 +183,53 @@ def plot_oscillations(alphas, save_fig=False, save_name=None):
     plt.setp(ax.get_yticklabels(), fontsize=12)
 
     # Set the top and right side frame & ticks off
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
+    _set_lr_spines(ax, 2)
 
-    # Set linewidth of remaining spines
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['bottom'].set_linewidth(2)
-
-    if save_fig:
-        save_name = 'plts/' + save_name + '.pdf'
-        plt.savefig(save_name, bbox_inches='tight', dpi=300)
+    _save_fig(save_fig, save_name)
 
 
-def plot_background(bgs, save_fig=False, save_name=None):
+def plot_background(bgs, control_offset=False, save_fig=False, save_name=None):
     """Plot background components, comparing between groups."""
 
     n_subjs = bgs.shape[0]
 
     # Set offset to be zero across all PSDs
     tbgs = np.copy(bgs)
-    tbgs[:, 0] = 1
+    if control_offset:
+        tbgs[:, 0] = 1
 
-    # Initialize figure
-    fig, ax = plt.subplots(figsize=[6, 6])
+    fig, ax = plt.subplots(figsize=[8, 6])
 
     # Get frequency axis (x-axis)
-    fs = np.arange(3, 40, 0.1)
+    fs = np.arange(1, 45, 0.1)
 
     # Create the background model from parameters
     bg_psds = np.empty(shape=[n_subjs, len(fs)])
     for ind, bg in enumerate(tbgs):
         bg_psds[ind, :] = expo_nk_function(fs, *tbgs[ind, :])
 
+    # Set whether to plot x-axis in log
     plt_log = False
     fs = np.log10(fs) if plt_log else fs
 
     # Plot each individual subject
     for ind in range(n_subjs):
-        lc = YOU_COL if ind in YNG_INDS else OLD_COL
+        lc = YNG_COL if ind in YNG_INDS else OLD_COL
         ax.plot(fs, bg_psds[ind, :], lc, alpha=0.2, linewidth=1.5)
 
     # Plot the average across all subjects, split up by age group
     you_avg = np.mean(bg_psds[YNG_INDS, :], 0)
     old_avg = np.mean(bg_psds[OLD_INDS, :], 0)
-    ax.plot(fs, you_avg, YOU_COL, linewidth=4, label='Young')
+    ax.plot(fs, you_avg, YNG_COL, linewidth=4, label='Young')
     ax.plot(fs, old_avg, OLD_COL, linewidth=4, label='Old')
 
+    # Shade regions of siginificant difference
+    avg_diffs, p_vals = bg_comps(fs, bg_psds)
+    sh_starts, sh_ends = get_pval_shades(fs, p_vals)
+    _plt_shade_regions(sh_starts, sh_ends)
+
+    # Plot limits & labels
+    ax.set_xlim([min(fs)-0.5, max(fs) + 0.5])
     ax.set_xlabel('Frequency', {'fontsize': 14, 'fontweight':'bold'})
     ax.set_ylabel('Power', {'fontsize': 14, 'fontweight':'bold'})
 
@@ -224,56 +238,27 @@ def plot_background(bgs, save_fig=False, save_name=None):
     plt.setp(ax.get_yticklabels(), fontsize=12)
 
     # Set the top and right side frame & ticks off
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-
-    # Set linewidth of remaining spines
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['bottom'].set_linewidth(2)
+    _set_lr_spines(ax, 2)
 
     plt.legend()
 
-    if save_fig:
-        save_name = 'plts/' + save_name + '.pdf'
-        plt.savefig(save_name, bbox_inches='tight', dpi=300)
+    _save_fig(save_fig, save_name)
 
 
-def plot_sl_band_diff(freqs, model_bgs, save_fig=False, save_name=None):
+def plot_sl_band_diff(freqs, avg_diffs, p_vals, save_fig=False, save_name=None):
     """Plot a comparison between specific frequency values from generated backgrounds.
 
     freqs - 1d vector of frequency values
-    model_bgs - 2d vector of [n_subjs, n_freqs]
+    avg_diffs - 1d vector of differences per frequency value (same len as freqs)
+    p_vals - 1d vector of p-values for each comparison  (same len as freqs)
     """
 
-    avg_diff = []
-    p_vals = []
-
-    for f_val in model_bgs.T:
-        avg_diff.append(np.mean(f_val[YNG_INDS] - np.mean(f_val[OLD_INDS])))
-        p_vals.append(ttest_ind(f_val[YNG_INDS], f_val[OLD_INDS])[1])
-
-    # Initialize plot
     fig, ax = plt.subplots(figsize=[8, 6])
 
     ax.set_xlim([0, max(freqs) + 0.5])
     ax.set_ylim([-0.75, 0.75])
 
-    plt.plot(freqs, avg_diff, '.', markersize=12)
-
-    pst = None
-    pen = None
-
-    # Find p-value ranges to shade in
-    #  Note: this presumes starts significant, gets unsignificant, gets significant again
-    for f_val, p_val in zip(freqs, p_vals):
-        if p_val < 0.05:
-            if not pst and pen:
-                pst = f_val
-        else:
-            if not pen:
-                pen = f_val
+    plt.plot(freqs, avg_diffs, '.', markersize=12)
 
     plt.title('\'Periodic\' Differences from Aperiodic Component', {'fontsize': 16, 'fontweight': 'bold'})
     ax.set_xlabel('Frequency', {'fontsize': 14, 'fontweight': 'bold'})
@@ -284,28 +269,21 @@ def plot_sl_band_diff(freqs, model_bgs, save_fig=False, save_name=None):
     plt.setp(ax.get_yticklabels(), fontsize=12)
 
     # Set the top and right side frame & ticks off
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-
-    # Set linewidth of remaining spines
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['bottom'].set_linewidth(2)
+    _set_lr_spines(ax, 2)
 
     # Add shading for statistically significant different regions
-    sh_starts = [0, pst-0.5]
-    sh_ends = [pen+0.5, max(freqs) + 0.5]
-    for shs, she in zip(sh_starts, sh_ends):
-        plt.axvspan(shs, she, color='r', alpha=0.2, lw=0)
+    sh_starts, sh_ends = get_pval_shades(freqs, p_vals)
+    _plt_shade_regions(sh_starts, sh_ends)
 
-    if save_fig:
-        save_name = 'plts/' + save_name + '.pdf'
-        plt.savefig(save_name, bbox_inches='tight', dpi=300)
+    _save_fig(save_fig, save_name)
 
 
 def plot_overlap(m1, m2, std1, std2, save_fig=False, save_name=None):
-    """Visualize the overlap of two gaussians."""
+    """Visualize the overlap of two gaussians.
+
+    m1 & std1 : define the average alpha
+    m2 & std2 : define the average alpha
+    """
 
     # Get point of overlap
     r = get_intersect(m1, m2, std1, std2)
@@ -319,8 +297,8 @@ def plot_overlap(m1, m2, std1, std2, save_fig=False, save_name=None):
     x = np.linspace(0, 20, 1000)
     step = x[1] - x[0]
 
-    plot1 = plt.plot(x, norm.pdf(x, m1, std1), 'grey', lw=2.5, label='Canonical')
-    plot2 = plt.plot(x, norm.pdf(x, m2, std2), 'black', lw=2.5, label='Average')
+    plot1 = plt.plot(x, norm.pdf(x, m1, std1), 'grey', lw=2.5, label='Average')
+    plot2 = plt.plot(x, norm.pdf(x, m2, std2), 'black', lw=2.5, label='Canonical')
     #plot3 = plt.plot(r, norm.pdf(r, m1, std1), markersize=22)#, 'o')
 
     # Shade in overlapping area
@@ -330,20 +308,11 @@ def plot_overlap(m1, m2, std1, std2, save_fig=False, save_name=None):
                      alpha=0.7, color='#d10c29', lw=0)
 
     # Set the top and right side frame & ticks off
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-
-    # Set linewidth of remaining spines
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['bottom'].set_linewidth(2)
+    _set_lr_spines(ax, 2)
 
     plt.legend(fontsize=12)
 
-    if save_fig:
-        save_name = 'plts/' + save_name + '.pdf'
-        plt.savefig(save_name, bbox_inches='tight', dpi=300)
+    _save_fig(save_fig, save_name)
 
 
 def plot_task_response(dat, title='', save_fig=False, save_name=None):
@@ -363,7 +332,7 @@ def plot_task_response(dat, title='', save_fig=False, save_name=None):
     avg_dat_yng = avg_func(avg_func(t_dat_yng, 0), 0)
     avg_dat_old = avg_func(avg_func(t_dat_old, 0), 0)
 
-    plt.plot([0, 1, 2], avg_dat_yng, YOU_COL, lw=2.5, label='Yng')
+    plt.plot([0, 1, 2], avg_dat_yng, YNG_COL, lw=2.5, label='Yng')
     plt.plot([0, 1, 2], avg_dat_old, OLD_COL, lw=2.5, label='Old')
 
     plt.title(title + ' Response', fontsize=16, fontweight='bold')
@@ -374,14 +343,39 @@ def plot_task_response(dat, title='', save_fig=False, save_name=None):
     plt.setp(ax.get_xticklabels(), fontsize=14, fontweight='bold')
     plt.setp(ax.get_yticklabels(), fontsize=12)
 
+    plt.legend(fontsize=12)
+
+    _set_lr_spines(ax)
+
+    _save_fig(save_fig, save_name)
+
+###################################################################################################
+###################################################################################################
+
+def _save_fig(save_fig, save_name):
+
+    if save_fig:
+        save_name = 'plts/' + save_name + '.pdf'
+        plt.savefig(save_name, bbox_inches='tight', dpi=300)
+
+
+def _set_lr_spines(ax, lw=None):
+    """   """
+
     # Set the top and right side frame & ticks off
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
 
-    plt.legend(fontsize=12)
+    # Set linewidth of remaining spines
+    if lw:
+        ax.spines['left'].set_linewidth(lw)
+        ax.spines['bottom'].set_linewidth(lw)
 
-    if save_fig:
-        save_name = 'plts/' + save_name + '.pdf'
-        plt.savefig(save_name, bbox_inches='tight', dpi=300)
+
+def _plt_shade_regions(shade_starts, shade_ends):
+    """Shade in regions of a plot."""
+
+    for shs, she in zip(shade_starts, shade_ends):
+        plt.axvspan(shs, she, color='r', alpha=0.2, lw=0)
--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,4 +1,4 @@
-""""   """
+""""Helper / utility functions for EEG-FOOOF."""
 
 import numpy as np
 from scipy.stats import norm
@@ -28,3 +28,43 @@ def get_overlap(intersect, m1, m2, std1, std2):
     """Get the percent overlap of two gaussians, given their definitions, and intersection point."""
 
     return norm.cdf(intersect, m2, std2) + (1. - norm.cdf(intersect, m1, std1))
+
+
+def calc_bg_comps(freqs, model_bgs):
+    """Calculate point by point comparison of power per frequency, from background components.
+
+    freqs: vector of frequency values
+    model_bgs: power spectra generated as just the background component
+    """
+
+    avg_diffs = []
+    p_vals = []
+
+    for f_val in model_bgs.T:
+        avg_diffs.append(np.mean(f_val[YNG_INDS] - np.mean(f_val[OLD_INDS])))
+        p_vals.append(ttest_ind(f_val[YNG_INDS], f_val[OLD_INDS])[1])
+
+    return avg_diffs, p_vals
+
+
+def get_pval_shades(freqs, p_vals):
+    """Find p-value ranges to shade in.
+
+    Note:
+        This approach presumes starts significant, gets unsignificant, gets significant again.
+    """
+
+    pst, pen = None, None
+
+    for f_val, p_val in zip(freqs, p_vals):
+        if p_val < 0.05:
+            if not pst and pen:
+                pst = f_val
+        else:
+            if not pen:
+                pen = f_val
+
+    sh_starts = [0, pst-0.5]
+    sh_ends = [pen+0.5, max(freqs) + 0.5]
+
+    return sh_starts, sh_ends
