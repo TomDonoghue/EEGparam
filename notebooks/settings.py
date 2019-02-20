f545dc3b974f85202be102c2fb51555dd1defb45
TomDonoghue <tdonoghue@ucsd.edu>
2019-02-20 15:25:38 -0800

    file dump

--- notebooks/settings.py
+++ notebooks/settings.py
@@ -3,5 +3,8 @@
 YNG_INDS = list(range(14, 31))
 OLD_INDS = list(range(0, 14))
 
+# Hack to drop problem subject
+#YNG_INDS = list(set(YNG_INDS) - set([21]))
+
 YNG_COL = "#0d82c1"
 OLD_COL = "#239909"
